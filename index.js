37bc38de0bd86a0a028c83b614c182023cb6f7e5
kccasey91 <kennethcaseyjr8@gmail.com>
2025-01-12 13:14:26 +0100

    I have updated the functions and added DOM methods so the game can be played in the browser instead of the console.

--- index.js
+++ index.js
@@ -1,44 +1,69 @@
-function getComputerChoice(){
-    let randomNum = Math.random();
-    
-    if(randomNum < .333) {
-        return 'rock';
-    } else if(randomNum > .332 && randomNum < .666) {
-        return 'paper';
-    } else return 'scissors';
-}
+function startGame(){
 
-function getHumanChoice(){
-    let user = prompt('Rock, Paper or Scissors?');
-    return user.toLowerCase();
-}
+let humanScore = 0;
+let cpuScore = 0;
+let roundCounter = 0;
+const maxRounds = 5;
 
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-    function playRound(humanChoice, computerChoice){
-        if((computerChoice === 'rock' && humanChoice === 'scissors') ||
-           (computerChoice === 'paper' && humanChoice === 'rock') ||
-           (computerChoice === 'scissors' && humanChoice === 'paper')) {
-            computerScore++;
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
-           } else if ((humanChoice === 'rock' && computerChoice === 'scissors') ||
-                      (humanChoice === 'paper' && computerChoice === 'rock') ||
-                      (humanChoice === 'scissors' && computerChoice === 'paper')) {
-                        humanScore++;
-                        console.log(`You win! ${humanChoice} beats ${computerChoice}`)
-                      } else if (computerChoice === humanChoice) {
-                        console.log('Tie Game!');
-                      }
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      if(roundCounter < maxRounds) {
+        const cpuChoice = getCpuChoice();
+        playRound(button.id, cpuChoice);
+
+        if (roundCounter === maxRounds) {
+          displayResults();
+          disableButtons(buttons);
+  
     }
+    });
+});
 
-    for(let i = 0; i < 5; i++) {
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
+function playRound(humanChoice, computerChoice){
+  const displayPlayerScore = document.querySelector('.player-score');
+  const displayCpuScore = document.querySelector('.cpu-score');
+  const displayText = document.querySelector('.display-text');
 
-        playRound(humanChoice, computerChoice);
-    }
+  if((computerChoice === 'rock' && humanChoice === 'scissors') ||
+     (computerChoice === 'paper' && humanChoice === 'rock') ||
+     (computerChoice === 'scissors' && humanChoice === 'paper')) {
+      cpuScore++;
+      displayCpuScore.textContent = `${cpuScore}`;
+      displayText.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
+      return cpuScore;
+     } else if ((humanChoice === 'rock' && computerChoice === 'scissors') ||
+                (humanChoice === 'paper' && computerChoice === 'rock') ||
+                (humanChoice === 'scissors' && computerChoice === 'paper')) {
+                  humanScore++;
+                  displayPlayerScore.textContent = `${humanScore}`;
+                  displayText.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+                  return humanScore;
+                } else if (computerChoice === humanChoice) {
+                  displayText.textContent = 'Tie Game!';
+                }
 }
 
-playGame();
+function displayResults(){
+  
+}
 
+
+function getCpuChoice(){
+  let randomNum = Math.random();
+  
+  if(randomNum < .333) {
+      return 'rock';
+  } else if(randomNum > .332 && randomNum < .666) {
+      return 'paper';
+  } else return 'scissors';
+}
+}
+
+const start = document.querySelector('.start-button');
+
+start.addEventListener('click', () => {
+  startGame();
+  start.disabled = true;
+})
